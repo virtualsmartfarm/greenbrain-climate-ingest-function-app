f27edddc021e7344158f672ec4267bdef713c652
Matt Cox <matthew.cox@agriculture.vic.gov.au>
2022-02-25 22:29:43 +1100

    First commit

---
+++ Hamilton_SensorGroup13347_15MinData_TimerTrigger/__init__.py
@@ -0,0 +1,114 @@
+import datetime
+import logging
+import azure.functions as func
+import json
+import pendulum
+import requests
+import os
+from azure.cosmos import CosmosClient, PartitionKey, exceptions
+import pandas as pd
+try:
+    greenbrain_username=os.environ['GREENBRAIN_USERNAME']
+except:
+    pass
+greenbrain_username="matthew.cox@ecodev.vic.gov.au"
+try:
+    greenbrain_password=os.environ['GREENBRAIN_PASSWORD']
+except:
+    pass
+greenbrain_password="FrlyuzucRIm0"
+try:
+    cosmosdb_key_vsfdatawatch=os.environ['COSMOSDB_KEY_VSFDATAWATCH']
+except:
+    pass
+cosmosdb_key_vsfdatawatch='0E8mMnMMb3LzIgQkUFlpcjiyH8R2rJHOpq8ejuWG14hiGxehLYzROEuHSyaYIV1yj0yBgQrSm6S6Gb7dvnhsHA=='
+greenbrain_endpoint='https://api.greenbrain.net.au/v3'
+auth_login = '/auth/login'
+bootstrap_uri = '/bootstrap'
+cosmosdb_endpoint='https://vsfdatawatch.documents.azure.com:443/'
+client = CosmosClient(url=cosmosdb_endpoint, credential=cosmosdb_key_vsfdatawatch)
+database_name = 'scheduled_ingest'
+database = client.get_database_client(database_name)
+container_name = 'greenbrain'
+def main(mytimer: func.TimerRequest) -> None:
+    utc_timestamp = datetime.datetime.utcnow().replace(
+        tzinfo=datetime.timezone.utc).isoformat()
+    api_payload = {
+        'email': greenbrain_username, 
+        'password': greenbrain_password
+    }
+    # print(api_payload)
+    login_header = {
+        'Content-type': 'application/json'
+    }
+    # print(login_header)
+    try:
+        container = database.create_container(id=container_name, partition_key=PartitionKey(path="/date/month"))
+    except exceptions.CosmosResourceExistsError:
+        container = database.get_container_client(container_name)
+    except exceptions.CosmosHttpResponseError:
+        raise
+    # https://api.greenbrain.net.au/v3/docs
+    response = requests.post(greenbrain_endpoint+auth_login, headers=login_header, data=json.dumps(api_payload))
+    # print(response.text)
+    greenbrain_token = response.json()['token']
+    # print(greenbrain_token)
+    bearer_token = str('Bearer '+ greenbrain_token)
+    # print(bearer_token)
+    bootstrap_header = {
+        'Authorization': bearer_token
+    }
+    # print(bootstrap_header)
+    bootstrap_response = requests.get(greenbrain_endpoint + bootstrap_uri, headers=bootstrap_header)
+    bootstrap_response = json.loads(bootstrap_response.text)
+    # print(bootstrap_response)
+    # MEA weather station in Hamilton ['systems'][1] ['stations'][0] has systems timezone set in Australia/Melbourne, get this from bootstrap
+    device_timezone = bootstrap_response['systems'][1]['stations'][0]['timezone']
+    # print(device_timezone)
+    hamilton_13347_longitude = bootstrap_response['systems'][1]['stations'][0]['longitude']
+    # print(hamilton_13347_longitude)
+    hamilton_13347_latitude = bootstrap_response['systems'][1]['stations'][0]['latitude']
+    # print(hamilton_13347_latitude)
+    yesterday_timestamp = pendulum.now(device_timezone).end_of('day').subtract(days=1).in_timezone('UTC').format('YYYY-MM-DDTHH:mm:ss')+'Z' # format for Cosmos DQ query 1970-01-01 00:00:01
+    # print(yesterday_timestamp)
+    yesterdays_date = pendulum.parse(yesterday_timestamp).format('YYYY-MM-DD') # format for Greenbrain API 1970-01-01
+    # print(yesterdays_date)
+    def payload_df(df_name, metric_title, sensor_title):
+        df_name.rename(columns={'time': 'vendor_timestamp'}, inplace=True)
+        df_name['timestamp_utc'] = df_name['vendor_timestamp'].apply(lambda x: pendulum.parse(x, tz=device_timezone).in_timezone('UTC').format('YYYY-MM-DDTHH:mm:ss'))+'Z'
+        df_name["vendor"] = "greenbrain"
+        df_name['type'] = "climate"
+        df_name['metric'] = metric_title
+        df_name['sensor'] = sensor_title
+        df_name['location'] = "hamilton_smartfarm"
+        df_name['coordinate'] = f'{{"latitude": {hamilton_13347_latitude}, "longitude": {hamilton_13347_longitude}}}'
+        df_name['coordinate'] = df_name['coordinate'].apply(lambda x: json.loads(x))
+        df_name['time'] = df_name["timestamp_utc"].apply(lambda x: pendulum.parse(x).format('[{"sec": ]s [,"min": ] m [,"hour":] H[}]'))
+        df_name['time'] = df_name["time"].apply(lambda x: json.loads(x))
+        df_name['date'] = df_name["timestamp_utc"].apply(lambda x: pendulum.parse(x).format('[{"day": ]D [,"month": ] M [,"year":] YYYY[}]'))
+        df_name['date'] = df_name["date"].apply(lambda x: json.loads(x))
+        df_name['id'] = df_name['timestamp_utc'].apply(lambda x: pendulum.parse(x).format('X[_]') + sensor_title)
+        for i in range(0,df_name.shape[0]):
+            data_dict = dict(df_name.iloc[i,:])
+            data_dict = json.dumps(data_dict)
+            # print(data_dict)
+            container.upsert_item(json.loads(data_dict)) # comment this out to stop upload to Cosmos Db
+        logging.info('Records inserted successfully into CosmosDB.')
+    response=requests.get("{}/sensor-groups/{}/readings?date={}".format(greenbrain_endpoint, 13347, yesterdays_date), headers=bootstrap_header)
+    response_13347_str = json.loads(response.text)
+    # print(response_13347_str)
+    # Minimum temperature sensor reading from 'sensor groups' 13347
+    response_90893_min_df = pd.json_normalize(response_13347_str['sensorTypes']['airTemperature']['sensors']['minimum']['readings'])
+    payload_df(response_90893_min_df, 'degree_celsius', '90893airtempmin')
+    # Average temperature sensor reading from 'sensor groups' 13347
+    response_90894_avg_df = pd.json_normalize(response_13347_str['sensorTypes']['airTemperature']['sensors']['average']['readings'])
+    payload_df(response_90894_avg_df, 'degree_celsius', '90894airtempavg')
+    # Maximum temperature sensor reading from 'sensor groups' 13347
+    response_90895_max_df = pd.json_normalize(response_13347_str['sensorTypes']['airTemperature']['sensors']['maximum']['readings'])
+    payload_df(response_90895_max_df, 'degree_celsius', '90895airtempmax')
+    # Rainfall sensor reading from 'sensor groups' 13347
+    response_90906_rainfall_df = pd.json_normalize(response_13347_str['sensorTypes']['rainfall']['sensors']['rainfall']['readings'])
+    payload_df(response_90906_rainfall_df, 'mm', '90906rainfall')
+    if mytimer.past_due:
+        logging.info('The timer is past due!')
+    logging.info('Python timer trigger function ran at %s', utc_timestamp)
---
+++ Mildura_SensorGroup8713_15MinData_TimerTrigger/__init__.py
@@ -0,0 +1,114 @@
+import datetime
+import logging
+import azure.functions as func
+import json
+import pendulum
+import requests
+import os
+from azure.cosmos import CosmosClient, PartitionKey, exceptions
+import pandas as pd
+try:
+    greenbrain_username=os.environ['GREENBRAIN_USERNAME']
+except:
+    pass
+greenbrain_username="matthew.cox@ecodev.vic.gov.au"
+try:
+    greenbrain_password=os.environ['GREENBRAIN_PASSWORD']
+except:
+    pass
+greenbrain_password="FrlyuzucRIm0"
+try:
+    cosmosdb_key_vsfdatawatch=os.environ['COSMOSDB_KEY_VSFDATAWATCH']
+except:
+    pass
+cosmosdb_key_vsfdatawatch='0E8mMnMMb3LzIgQkUFlpcjiyH8R2rJHOpq8ejuWG14hiGxehLYzROEuHSyaYIV1yj0yBgQrSm6S6Gb7dvnhsHA=='
+greenbrain_endpoint='https://api.greenbrain.net.au/v3'
+auth_login = '/auth/login'
+bootstrap_uri = '/bootstrap'
+cosmosdb_endpoint='https://vsfdatawatch.documents.azure.com:443/'
+client = CosmosClient(url=cosmosdb_endpoint, credential=cosmosdb_key_vsfdatawatch)
+database_name = 'scheduled_ingest'
+database = client.get_database_client(database_name)
+container_name = 'greenbrain'
+def main(mytimer: func.TimerRequest) -> None:
+    utc_timestamp = datetime.datetime.utcnow().replace(
+        tzinfo=datetime.timezone.utc).isoformat()
+    api_payload = {
+        'email': greenbrain_username, 
+        'password': greenbrain_password
+    }
+    # print(api_payload)
+    login_header = {
+        'Content-type': 'application/json'
+    }
+    # print(login_header)
+    try:
+        container = database.create_container(id=container_name, partition_key=PartitionKey(path="/date/month"))
+    except exceptions.CosmosResourceExistsError:
+        container = database.get_container_client(container_name)
+    except exceptions.CosmosHttpResponseError:
+        raise
+    # https://api.greenbrain.net.au/v3/docs
+    response = requests.post(greenbrain_endpoint+auth_login, headers=login_header, data=json.dumps(api_payload))
+    # print(response.text)
+    greenbrain_token = response.json()['token']
+    # print(greenbrain_token)
+    bearer_token = str('Bearer '+ greenbrain_token)
+    # print(bearer_token)
+    bootstrap_header = {
+        'Authorization': bearer_token
+    }
+    # print(bootstrap_header)
+    bootstrap_response = requests.get(greenbrain_endpoint + bootstrap_uri, headers=bootstrap_header)
+    bootstrap_response = json.loads(bootstrap_response.text)
+    # print(bootstrap_response)
+    # MEA weather station in Mildura (systems 3) has systems timezone set in Australia/Adelaide, get this from bootstrap
+    device_timezone = bootstrap_response['systems'][3]['stations'][0]['timezone']
+    # print(device_timezone)
+    mildura_8713_longitude = bootstrap_response['systems'][3]['stations'][0]['longitude']
+    print(mildura_8713_longitude)
+    mildura_8713_latitude = bootstrap_response['systems'][3]['stations'][0]['latitude']
+    print(mildura_8713_latitude)
+    yesterday_timestamp = pendulum.now(device_timezone).end_of('day').subtract(days=1).in_timezone('UTC').format('YYYY-MM-DDTHH:mm:ss')+'Z' # format for Cosmos DQ query 1970-01-01 00:00:01
+    # print(yesterday_timestamp)
+    yesterdays_date = pendulum.parse(yesterday_timestamp).format('YYYY-MM-DD') # format for Greenbrain API 1970-01-01
+    # print(yesterdays_date)
+    def payload_df(df_name, metric_title, sensor_title):
+        df_name.rename(columns={'time': 'vendor_timestamp'}, inplace=True)
+        df_name['timestamp_utc'] = df_name['vendor_timestamp'].apply(lambda x: pendulum.parse(x, tz=device_timezone).in_timezone('UTC').format('YYYY-MM-DDTHH:mm:ss'))+'Z'
+        df_name["vendor"] = "greenbrain"
+        df_name['type'] = "climate"
+        df_name['metric'] = metric_title
+        df_name['sensor'] = sensor_title
+        df_name['location'] = "mildura_smartfarm"
+        df_name['coordinate'] = f'{{"latitude": {mildura_8713_latitude}, "longitude": {mildura_8713_longitude}}}'
+        df_name['coordinate'] = df_name['coordinate'].apply(lambda x: json.loads(x))
+        df_name['time'] = df_name["timestamp_utc"].apply(lambda x: pendulum.parse(x).format('[{"sec": ]s [,"min": ] m [,"hour":] H[}]'))
+        df_name['time'] = df_name["time"].apply(lambda x: json.loads(x))
+        df_name['date'] = df_name["timestamp_utc"].apply(lambda x: pendulum.parse(x).format('[{"day": ]D [,"month": ] M [,"year":] YYYY[}]'))
+        df_name['date'] = df_name["date"].apply(lambda x: json.loads(x))
+        df_name['id'] = df_name['timestamp_utc'].apply(lambda x: pendulum.parse(x).format('X[_]') + sensor_title)
+        for i in range(0,df_name.shape[0]):
+            data_dict = dict(df_name.iloc[i,:])
+            data_dict = json.dumps(data_dict)
+            # print(data_dict)
+            container.upsert_item(json.loads(data_dict)) # comment this out to stop upload to Cosmos Db
+        logging.info('Records inserted successfully into CosmosDB.')
+    response=requests.get("{}/sensor-groups/{}/readings?date={}".format(greenbrain_endpoint, 8713, yesterdays_date), headers=bootstrap_header)
+    response_8713_str = json.loads(response.text)
+    # print(response_8713_str)
+    # Minimum temperature sensor reading from 'sensor groups' 8713
+    response_59213_min_df = pd.json_normalize(response_8713_str['sensorTypes']['airTemperature']['sensors']['minimum']['readings'])
+    payload_df(response_59213_min_df, 'degree_celsius', '59213airtempmin')
+    # Average temperature sensor reading from 'sensor groups' 8713
+    response_59214_avg_df = pd.json_normalize(response_8713_str['sensorTypes']['airTemperature']['sensors']['average']['readings'])
+    payload_df(response_59214_avg_df, 'degree_celsius', '59214airtempavg')
+    # Maximum temperature sensor reading from 'sensor groups' 8713
+    response_59215_max_df = pd.json_normalize(response_8713_str['sensorTypes']['airTemperature']['sensors']['maximum']['readings'])
+    payload_df(response_59215_max_df, 'degree_celsius', '59215airtempmax')
+    # Rainfall sensor reading from 'sensor groups' 8713
+    response_59213_rainfall_df = pd.json_normalize(response_8713_str['sensorTypes']['rainfall']['sensors']['rainfall']['readings'])
+    payload_df(response_59213_rainfall_df, 'mm', '59226rainfall')
+    if mytimer.past_due:
+        logging.info('The timer is past due!')
+    logging.info('Python timer trigger function ran at %s', utc_timestamp)
