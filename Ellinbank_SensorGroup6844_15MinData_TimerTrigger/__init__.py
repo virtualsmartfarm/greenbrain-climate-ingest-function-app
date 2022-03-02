5a172840d2dd6ea00d3bb3a31689ab2c06ddf306
Matt Cox <matthew.cox@agriculture.vic.gov.au>
2022-03-02 11:00:21 +1100

    Data written to Greenbrain container not folder.

--- Ellinbank_SensorGroup6844_15MinData_TimerTrigger/__init__.py
+++ Ellinbank_SensorGroup6844_15MinData_TimerTrigger/__init__.py
@@ -136,9 +136,9 @@ def main(mytimer: func.TimerRequest) -> None:
         payload_csv=payload_df.to_csv(index=False)
         # print(payload_csv)
         if payload_csv != '[]':
-            write_response(f'greenbrain-{sensor_name}-{metric}.csv', payload_csv, 'greenbrain', 'curated', 'avrvsfdatawatch', adls_avrvsfdatawatch_credentials) 
+            write_response(f'greenbrain-{sensor_name}-{metric}.csv', payload_csv, 'greenbrain', '.', 'avrvsfdatawatch', adls_avrvsfdatawatch_credentials) 
         else:
-            logging.info('The query to Cosmos DB did not return any data. No data added to the curated folder during the past 24 hours.')
+            logging.info('The query to Cosmos DB did not return any data. No data added to the Greenbain container during the past 24 hours.')
     # Query Cosmos Db to create a CSV of all records
     sensor_query('airtempmin46977', 'ellinbank_smartfarm', 'degree')
     sensor_query('airtempavg46978', 'ellinbank_smartfarm', 'degree')
